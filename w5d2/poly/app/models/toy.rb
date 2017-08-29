fc087efb9f5cfcf03327514a8fd85c7f5479c9eb
brian sohn <sohnbrian@gmail.com>
2017-08-28 19:50:13 -0700

    polymorphism

---
+++ w5d2/poly/app/models/toy.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string
+#  toyable_id   :integer
+#  toyable_type :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Toy < ApplicationRecord
+  validates :name, uniqueness: { scope: [:toyable] }
+  belongs_to :toyable, polymorphic: true
+
+  
+
+end
